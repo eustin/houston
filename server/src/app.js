bba454cff2890acb9e953410e10fbbfce5a8d862
Justin Evans <eustin.jvans@gmail.com>
2022-06-09 19:21:31 +1000

    server: mount launches router on /launches

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 
+const launchesRouter = require("./routes/launches.router");
+
 app = express();
 app.use(express.json());
+
+app.use("/launches", launchesRouter);
 
 module.exports = app;
