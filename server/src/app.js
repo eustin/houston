1d95bc38d8ea9b099fa3f750c6d45bb398d5e2f8
Justin Evans <eustin.jvans@gmail.com>
2022-06-14 07:08:32 +1000

    server: allow client access to server by specifying client orign nad using cors middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 
 const launchesRouter = require("./routes/launches.router");
 
 app = express();
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:3000" }));
 
 app.use("/launches", launchesRouter);
 
