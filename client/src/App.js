8c17a72f7ff60817acccb39922ee321a70c4665b
Justin Evans <eustin.jvans@gmail.com>
2022-06-14 07:10:02 +1000

    client: show latest launches in App.js for dev purposes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+import { httpGetLatestLaunches } from "./api/houstonBackend";
 
 function App() {
+  // todo: refactor all of this dev code
+  const [launches, setLaunches] = useState([]);
+
+  useEffect(() => {
+    async function getLaunches() {
+      const response = await httpGetLatestLaunches();
+      setLaunches(response);
+    }
+    getLaunches();
+  }, []);
+
   return (
-    <div>Welcome to Houston</div>
+    <div>
+      <div>Welcome to Houston!</div>
+      <div>Here are your upcoming launches:</div>
+      <ul>
+        {launches.map((launch) => (
+          <li key={launch.id}>{launch.name}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
